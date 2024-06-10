528915a7bac438f3dc036abde457cbffa1f6980d
hgodinho <henrique@hgod.in>
2024-06-10 20:09:44 -0300

    fix(gen): adjust packageJsonTemplate

--- tools/gen/lib/packageJsonTemplate.js
+++ tools/gen/lib/packageJsonTemplate.js
@@ -2,14 +2,16 @@ const packageJsonTemplate = (type, name, description, author, license) => {
     return {
         name: `@elucidario/${type}-${name}`,
         version: "0.1.0",
-        description,
-        author,
+        keywords: [],
         license,
+        author,
+        description,
         main: "index.js",
         scripts: {
-            test: 'echo "Error: no test specified" && exit 1',
+            build: "tsc",
+            clean: "rm -rf dist",
+            dev: "tsc -w",
         },
-        keywords: [],
         devDependencies: {
             "@elucidario/tool-tsconfig": "workspace:^",
         },
