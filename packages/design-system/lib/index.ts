eecc7a83a017447d5b17f01a01469c6224ee71ce
Henrique Godinho <ola@hgod.in>
2025-03-07 14:31:19 -0300

    feat(ds): export cva & VariantProps type

--- packages/design-system/lib/index.ts
+++ packages/design-system/lib/index.ts
@@ -3,3 +3,6 @@ export * from "./provider";
 export * from "./hooks";
 export * from "./utils";
 export * from "./templates";
+
+import { cva, type VariantProps } from "class-variance-authority";
+export { cva, VariantProps };
