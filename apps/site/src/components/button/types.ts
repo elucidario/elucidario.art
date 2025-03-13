33d7de914f34c543344c3dd8b20b03ca2e682fc0
Henrique Godinho <ola@hgod.in>
2025-03-12 21:56:21 -0300

    fix(site): simplify button types

--- apps/site/src/components/button/types.ts
+++ apps/site/src/components/button/types.ts
@@ -1,8 +1,5 @@
-import React, { AriaAttributes } from "react";
-
 export type ButtonProps<T = unknown> = React.PropsWithChildren<
     React.ButtonHTMLAttributes<HTMLButtonElement> &
-        AriaAttributes &
         T & {
             ref?: React.Ref<HTMLButtonElement>;
             asChild?: boolean;
