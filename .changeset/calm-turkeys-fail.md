8511070b8d7330444ca2e510123525c949a88a64
Henrique Godinho <ola@hgod.in>
2025-03-24 22:18:01 -0300

    feat(site): responsive typography

---
+++ .changeset/calm-turkeys-fail.md
@@ -0,0 +1,5 @@
+---
+"@elucidario/app-site": minor
+---
+
+responsive typography
--- apps/site/src/components/typography/Heading.tsx
+++ apps/site/src/components/typography/Heading.tsx
@@ -5,12 +5,12 @@ import { motion } from "motion/react";
 export const headingVariants = cva(["heading", "font-bold"], {
     variants: {
         level: {
-            1: ["text-3xl", "lg:text-5xl"],
-            2: ["text-2xl", "lg:text-4xl"],
-            3: ["text-xl", "lg:text-3xl"],
-            4: ["text-lg", "lg:text-2xl"],
-            5: ["text-lg", "lg:text-xl"],
-            6: ["text-base", "lg:text-lg"],
+            1: ["text-2xl", "md:text-4xl", "lg:text-5xl"],
+            2: ["text-xl", "md:text-3xl", "lg:text-4xl"],
+            3: ["text-lg", "md:text-2xl", "lg:text-3xl"],
+            4: ["text-base", "md:text-xl", "lg:text-2xl"],
+            5: ["text-base", "md:text-lg", "lg:text-xl"],
+            6: ["text-sm", "md:text-base", "lg:text-lg"],
         },
     },
     defaultVariants: {
