157297fe17b362e411e12dcce92d696ffad1c380
Henrique Godinho <ola@hgod.in>
2025-03-31 20:07:13 -0300

    fix(site): update base path

--- apps/site/vite.config.ts
+++ apps/site/vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-    base: "/elucidario.art/",
+    base: "/",
     server: {
         host: "0.0.0.0",
         port: 4000,
