f324fa0814c983eb83f222f056daf4b2ef3a91e5
Henrique Godinho <ola@hgod.in>
2025-03-31 20:36:04 -0300

    fix(site): update error page

--- apps/site/src/pages/_error/+Page.tsx
+++ apps/site/src/pages/_error/+Page.tsx
@@ -1,20 +1,17 @@
-import React from "react";
 import { usePageContext } from "vike-react/usePageContext";
+import { Main, Heading, Image } from "@/components";
 
 export default function Page() {
     const { is404 } = usePageContext();
-    if (is404) {
-        return (
-            <>
-                <h1>404 Page Not Found</h1>
-                <p>This page could not be found.</p>
-            </>
-        );
-    }
+
     return (
-        <>
-            <h1>500 Internal Server Error</h1>
-            <p>Something went wrong.</p>
-        </>
+        <Main className="h-screen flex flex-col items-center justify-center gap-10">
+            <Heading level={1} className="font-mono">
+                {is404
+                    ? "404 - Página não encontrada"
+                    : "500 - Erro interno do servidor"}
+            </Heading>
+            <Image src="gif/cat.gif" alt="Um gato fofo" width={300} />
+        </Main>
     );
 }
