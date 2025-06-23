f2981e05d10162669035be454c749c652950afa2
hgodinho <ola@hgod.in>
2025-06-23 14:42:41 -0300

    feat(core): cypher facade

---
+++ core/src/db/cypher.test.ts
@@ -0,0 +1,29 @@
+import { describe, expect, it } from "vitest";
+
+import { Cypher } from "./Cypher";
+
+describe("Cypher", () => {
+    const cypher = new Cypher();
+
+    it("should cypherBuilder", () => {
+        const query = cypher
+            .builder((Cypher) => {
+                const node = new Cypher.Node();
+                const pattern = new Cypher.Pattern(node, {
+                    labels: ["TestNode"],
+                });
+                const match = new Cypher.Match(pattern);
+                match.return(node);
+
+                return match;
+            })
+            .build();
+
+        expect(query.cypher).toBeDefined();
+        expect(query.params).toBeDefined();
+        expect(query.cypher.toString()).toBe(
+            `MATCH (this0:TestNode)
+RETURN this0`,
+        );
+    });
+});
