aa8f3a2fd73c015002ab4b5c848c27901e6e4307
hgodinho <ola@hgod.in>
2025-07-01 22:49:57 -0300

    refactor(core): endopointPath

--- core/src/utils/endpointPath.ts
+++ core/src/utils/endpointPath.ts
@@ -1,7 +1,32 @@
-export function endpointPath(...parts: string[]): string {
-    const path = parts
-        .filter((part) => part && part.trim() !== "")
-        .map((part) => part.replace(/^\//, "").replace(/\/$/, ""))
-        .join("/");
-    return `/${path}`;
+import { Routes } from "@/types";
+
+export type EndpointPathParams = {
+    before?: string[];
+    after?: string[];
+};
+
+export function endpointPath<T extends Record<string, string | string[]>>(
+    opts: Routes<T>,
+    endpointKey?: keyof T,
+): string {
+    const subPaths: Array<string | false> = [];
+
+    if (typeof endpointKey === "undefined") {
+        subPaths.push(false);
+    } else if (Array.isArray(opts.endpoints[endpointKey])) {
+        subPaths.push(...opts.endpoints[endpointKey]);
+    } else if (typeof opts.endpoints[endpointKey] === "string") {
+        subPaths.push(opts.endpoints[endpointKey]);
+    } else {
+        subPaths.push(false);
+    }
+
+    const paths = [
+        opts.prefix,
+        opts.version,
+        ...(typeof opts.path === "string" ? [opts.path] : opts.path),
+        ...subPaths,
+    ].filter(Boolean);
+
+    return `/${paths.join("/")}`;
 }
