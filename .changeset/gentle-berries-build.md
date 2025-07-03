544baaa6059de9f3c3c36f671d36cdc0cc503f50
hgodinho <ola@hgod.in>
2025-07-03 10:02:47 -0300

    fix(mdorim): fix schemas

---
+++ .changeset/gentle-berries-build.md
@@ -0,0 +1,5 @@
+---
+"@elucidario/mdorim": patch
+---
+
+fix mdorim schemas
--- packages/mdorim/src/schemas/core/index.ts
+++ packages/mdorim/src/schemas/core/index.ts
@@ -1,8 +1,17 @@
 import Definitions from "./definitions.json" with { type: "json" };
+import Config from "./config.json" with { type: "json" };
 import InvitedMember from "./invited-member.json" with { type: "json" };
 import TeamMember from "./team-member.json" with { type: "json" };
 import Team from "./team.json" with { type: "json" };
 import User from "./user.json" with { type: "json" };
 import Workspace from "./workspace.json" with { type: "json" };
 
-export { Definitions, InvitedMember, TeamMember, Team, User, Workspace };
+export {
+    Definitions,
+    Config,
+    InvitedMember,
+    TeamMember,
+    Team,
+    User,
+    Workspace,
+};
