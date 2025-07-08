77a977c6947f14e17049b6e2ecd9b6f99a60d32c
hgodinho <ola@hgod.in>
2025-07-07 22:03:44 -0300

    test(core): authz & validator

---
+++ core/src/tests/unit/application/validator.test.ts
@@ -0,0 +1,39 @@
+import { beforeAll, describe, expect, it } from "vitest";
+
+import { Validator } from "@/application/Validator";
+import { DefaultLocale, I18n, Mdorim } from "@elucidario/mdorim";
+import { User } from "@/domain/models/core";
+
+describe("Validator", () => {
+    let validator: Validator;
+
+    beforeAll(() => {
+        // Initialize the Validator instance before all tests
+        validator = new Validator(new Mdorim(new I18n(DefaultLocale)));
+    });
+
+    describe("should validate entity correctly", () => {
+        it("should throw an error if model is not defined", async () => {
+            validator.model = undefined; // Simulate no model set
+            await expect(
+                async () => await validator.validateEntity(),
+            ).rejects.toThrow(
+                "Model is not defined. Please set the model before getting the schema.",
+            );
+        });
+
+        it("should return true for valid entity", async () => {
+            validator.setModel(
+                new User({
+                    type: "User",
+                    username: "test_user",
+                    email: "test_user@example.com",
+                }),
+            );
+
+            const isValid = await validator.validateEntity();
+
+            expect(isValid).toBe(true);
+        });
+    });
+});
