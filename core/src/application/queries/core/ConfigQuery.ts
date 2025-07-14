c61dd5f8ba7a108f27251c08aaa06a9c9de8a9b1
hgodinho <ola@hgod.in>
2025-07-14 12:07:56 -0300

    fix(core): query clean-up

--- core/src/application/queries/core/ConfigQuery.ts
+++ core/src/application/queries/core/ConfigQuery.ts
@@ -1,4 +1,4 @@
 import { AQuery } from "../AQuery";
 import { Config, ConfigTypes } from "@elucidario/mdorim";
 
-export class ConfigQuery extends AQuery<Config<ConfigTypes>> {}
+export class ConfigQuery extends AQuery<Config<ConfigTypes>> { }
